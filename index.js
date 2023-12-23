d889e3199277196ec60e931e931ac9b2a3ac11d1
K0ftry <ericksepulveda94@hotmail.com>
2023-12-22 21:16:46 -0300

    3.15 DELETED implemented

--- index.js
+++ index.js
@@ -59,10 +59,13 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(element => element.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => {
+            console.log(error)
+        })
 })
 
 app.post('/api/persons', (request, response) => {
